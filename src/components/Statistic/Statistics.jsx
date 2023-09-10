2c0605f7fb7347360698919bf0e8f7fa375c25ba
Artem303 <125798638+Artem303@users.noreply.github.com>
2023-09-10 12:01:46 +0300

    first try

--- src/components/Statistic/Statistics.jsx
+++ src/components/Statistic/Statistics.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import CSS from './Statistics.css';
+import css from './Statistics.module.css';
 
 const Statistics = ({ good, neutral, bad, total, positivePercentage }) => {
   return (
-    <div>
+    <div className={css.statisticsContainer}>
       <h2>Statistics</h2>
-      <ul>
+      <ul className={css.statisticsList}>
         <li>Good: {good}</li>
         <li>Neutral: {neutral}</li>
         <li>Bad: {bad}</li>
